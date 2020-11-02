94cdf6abadc0ec3ba4fcfdabf6b9a35307396af7
giliover <giliover@hotmail.com>
2020-11-02 06:17:11 -0300

    added index to render

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-const soma = ( a, b ) => a + b
-console.log(soma( 2, 4))
+import React from "react"
+import { render } from "react-dom"
+import App from "./App"
+
+render(<App/> , document.getElementById("react_concepts"))
