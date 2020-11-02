89451dd003e292152cb10015d0e987fc8cac46b7
giliover <giliover@hotmail.com>
2020-11-02 09:53:02 -0300

    updaded with new functionalities

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from "react"
-import { Header } from './components/Header'
+import React, { useState } from "react"
+import { Header } from './components'
 
 export default function App(){
-    return (
-        <Header/>
-    ) 
+    const [projects, setProject] = useState(["Desenvolvimento web", "Front-end web"])
+
+    function handleAddProject(){
+        return setProject([...projects, `Novo projeto ${Date.now()}`])
+    }
+    
+    return(
+        <>
+        <Header title="Projects"/>
+            <ul>
+                {projects.map((project) => <li key={project}> {project} </li>)}
+            </ul>
+            <button onClick={handleAddProject}>Adcionar Projetos</button>
+        </>
+    )
 }
