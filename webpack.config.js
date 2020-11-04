eaa75cfc7345b05f2020a40f75756f5388187d57
giliover@hotmail.com <giliover@hotmail.com>
2020-11-04 05:44:47 -0300

    added css configurations

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,20 @@ module.exports = {
     module: {
         rules: [
             {
-                test:/\.js$/,
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                 }
-            }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ]
+            },
         ]
     }
 }
