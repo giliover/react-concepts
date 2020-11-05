7a6faa0158e371ba3fd48737f834387b5222bc21
giliover@hotmail.com <giliover@hotmail.com>
2020-11-04 21:56:17 -0300

    added axios to list repository

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [ repo, setRepo ] = useState([])
+
+  useEffect(() => {
+    api
+      .get( 'repositories' )
+      .then((response) => {
+        setRepo(response.data)        
+      })
+  }, [])
+
   async function handleAddRepository() {
     // TODO
   }
@@ -14,13 +24,16 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repo.map(({title, id}) => {
+          return(
+            <li key={id}>
+              { title }
+              <button onClick={() => handleRemoveRepository(id)}>
+                Remover
+              </button>
+            </li>          
+          )   
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
